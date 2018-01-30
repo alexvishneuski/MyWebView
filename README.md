a1e2ac461b180b4d2bce812f4f867391a1c3233d
alexvishneuski <alexvishneuski@gmail.com>
2018-01-30 21:51:22 +0300

    removed unused functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MyWebView
 webview mastering
+
+#useful links:
+1. https://developer.android.com/reference/android/webkit/WebView.html
+2. http://developer.alexanderklimov.ru/android/mybrowser.php
+3. http://startandroid.ru/ru/uroki/vse-uroki-spiskom/72-urok-32-pishem-prostoj-brauzer.html
+4. https://habrahabr.ru/company/mailru/blog/262167/
+5. https://developer.chrome.com/multidevice/webview/gettingstarted
--- app/src/main/java/com/github/alexvishneuski/mywebview/MainActivity.java
+++ app/src/main/java/com/github/alexvishneuski/mywebview/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     Button btnWeb;
     Button btnMap;
     Button btnCall;
@@ -18,33 +18,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnWeb = findViewById(R.id.btnWeb);
-        btnMap = findViewById(R.id.btnMap);
-        btnCall = findViewById(R.id.btnCall);
 
-        btnWeb.setOnClickListener(this);
-        btnMap.setOnClickListener(this);
-        btnCall.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent;
-        switch (v.getId()) {
-            case R.id.btnWeb:
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
                 startActivity(intent);
-                break;
-            case R.id.btnMap:
-                intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:55.754283,37.62002"));
-                startActivity(intent);
-                break;
-            case R.id.btnCall:
-                intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:12345"));
-                startActivity(intent);
-                break;
-        }
+            }
+        });
     }
 }
